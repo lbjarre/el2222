b433d5a6f9f9d1a3127856c497b3c3a10228f6a4
Lukas Bjarre <lbjarre@kth.se>
2017-09-13 13:42:47 +0200

    Created sinusoid changing light with PWM

--- Arduino_code/blink/blink.ino
+++ Arduino_code/blink/blink.ino
@@ -1,12 +1,19 @@
-int ledPin = 13;
+#include<Time.h>
+
+int ledPin = 9;
+float pi = 3.14;
+float f = 0.0005;
+float t;
+float val;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(500);
-  digitalWrite(ledPin, LOW);
-  delay(500);
+  t = millis();
+  val = (sin(2.0*pi*f*t) + 1) * 127;
+  analogWrite(ledPin, val);
+  Serial.println(val);
 }
